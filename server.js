775bf6880923c052556b9df173bff02b50c2a3ac
emadahmed97 <emad1997@gmail.com>
2016-10-16 01:45:46 -0400

    appointment scheduling and rounding

--- server.js
+++ server.js
@@ -26,13 +26,30 @@ mongoClient.connect('mongodb://emadahmed:emadhello123@ds031611.mlab.com:31611/me
 
 /// Handle GET request - Serve up home page.
 app.get('/home', (req, res) => {
-  db.collection('userText').find().toArray((err, result) => {
-    console.log('entered get')
+  //db.collection('userText').find().sort( { epoch_date: 1 } )
+  db.collection('userText').find().sort( { epoch_time: 1 } ).toArray((err, result) => {
     if (err) return console.log(err)
     // renders index.ejs
     res.render('index.ejs', {userText: result})
   })
+
+  //sdb.close();
+
 })
+
+
+var nextDate = function(dates, dateWant) {
+	for (var i = 0; i < dates.length; i++) {         // iterating through array of epoch times
+		if (Math.abs(dates[i] - dateWant) > 1800000) {
+			if (dateWant % 1800000 == 0) return dateWant; // if not within 30 minutes of another appointment and a multiple of 30 minutes, return user requested date
+			else {
+				break;
+			}
+		}
+	}
+	var sum = Math.abs(1800000 - (dateWant % 1800000)); // rounds to nearest time that is available if user requested time is not available
+	return dateWant + sum;
+};
 
 
 
@@ -40,40 +57,61 @@ app.get('/home', (req, res) => {
 /// Handle GET request - Send SMS to device.
 app.get('/message', (req, res) => {
   var twiml = new twilio.TwimlResponse()
-  twiml.message("Messaged sms . Thank you!")
-  console.log(req.query.Body)
   var text = String(req.query.Body).split(',')
-  var date = (text[1])
-  var time = (text[2])
+
+  var date = new Date(String(text[1])) // gets date (without time)
+
+  var time = String((text[2])) // gets time in hours:minutes format i.e. 5:06
+  time.split(':')
+
+  date.setHours(time.split(':')[0]) // hours  - 5
+  date.setMinutes(time.split(':')[1]) // minutes - 06
+
+  console.log("Full date is")
+  console.log(date);
+
+  var epochdate = date.getTime() // turns date into epoch time (easier for comparison)
+
+
+
+
+    var epochtimes=[]
+
+    var queryepoch = db.collection('userText').find({},{epoch_time:1, _id:0}).toArray((err, result) => {
+
+      // querying for only the epoch times in the database to see if the user-requested appointment is appropriate
+      epochtimes = result
+  console.log(result);
+
+
+      if (err) return console.log(err)
+    })
+
+console.log(epochdate)
+    console.log("next date is ");
+    var appointmentTime = new Date(nextDate(epochtimes,epochdate)) // calls helper function nextDate which returns rounded or requested time
+
+
   var reason = (text[3])
   var number = req.query.From
 
-  var appointment = {user_date: date, user_time: time, user_reason: reason, user_number: number};
-    // Insert some users
+  var appointment = {user_date: date, user_time: time, user_reason: reason, user_number: number, epoch_time: appointmentTime}; // create array to insert into database, with user fields
+
+    // Insert users
     info.insert([appointment], function (err, result) {
       if (err) {
         console.log(err);
       } else {
-        console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
       }
 
-      //res.writeHead(200, {'content-type': 'text/xml'})
-    //  res.end(twiml.toString())
+      twiml.message("Appointment booking successful. Your appointment is at: " + appointmentTime)
+
+      res.writeHead(200, {'content-type': 'text/xml'})
+    res.end(twiml.toString())
       //Close connection
-      //db.close();
     });
 
-    res.redirect('/home')
-
-})
 
 
 
-/// Handle POST request.
-app.post('/userText', (req, res) => {
-  db.collection('userText').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    //res.redirect('/')
-  })
 })
